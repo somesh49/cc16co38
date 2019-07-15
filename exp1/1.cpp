acda642490b653fd6fb21463b4e37b79dcbb269d
aakashkapdi <aakashkapdi17@gmail.com>
2019-07-15 15:51:48 +0530

    First c program

---
+++ exp1/1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char a[10];
+	int b,n;
+	cout<<"Enter a size of string"<<endl;
+	cin>>n;
+	cout<<"Enter string"<<endl;
+	for(int i=0;i<=n-1;i++)
+	{
+	cin>>a[i];
+    }
+	for(int i=0;i<=n-1;i++)
+	{
+		b=a[i];
+		if(b==a[i])
+		cout<<a[i]<<" is Integer"<<endl;
+		else
+		cout<<a[i]<<" is Character"<<endl;
+	
+	}
+}
